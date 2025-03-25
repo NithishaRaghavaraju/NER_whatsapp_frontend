8b3c18165c3cc6097ef5a15a492ddd9571a31f1c
NithishaRaghavaraju <nithisha2207@gmail.com>
2025-03-25 08:48:42 +0530

    capital checking

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 "use client";
 import { useState,useEffect } from "react";
 import Chats from "@/Components/Chats.jsx";
-import Sidebar from "@/Components/Sidebar.jsx";
+import Sidebar from "@/Components/SideBar.jsx";
 
 const Page = () => {
   const [showChat, setShowChat] = useState(false);
